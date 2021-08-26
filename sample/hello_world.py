89dc3889f5f256de34951858930531b1f6ba25c3
tu204f <david_yusupov@mail.ru>
2021-08-26 17:54:17 +0300

    tkinter - изумаем библиотку

---
+++ sample/hello_world.py
@@ -0,0 +1,5 @@
+from tkinter import *
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+window.mainloop()
---
+++ sample/label.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+lbl = Label(window,text="Привет")
+lbl.grid(column=0,row=0)
+window.mainloop()
