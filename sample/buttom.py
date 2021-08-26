7c5747a2b792d2b3c749e06ce658f2d2a2612d71
tu204f <david_yusupov@mail.ru>
2021-08-26 18:08:29 +0300

    Buttom - цвет готова

--- sample/buttom.py
+++ sample/buttom.py
@@ -2,7 +2,12 @@ from tkinter import *
 
 window = Tk()
 window.title("Добро пожаловать в приложение PythonRu")
+
 lbl = Label(window,text="Привет",font=("Arial Bold",50))
 lbl.grid(column=0,row=0)
+
+btn = Button(window,text="Не нажимать!")
+btn.grid(column=1,row=0)
+
 window.geometry('400x250')
 window.mainloop()
