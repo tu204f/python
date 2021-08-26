7c35698d3e278451110bcc05072c9b751f8b945f
tu204f <david_yusupov@mail.ru>
2021-08-26 18:21:05 +0300

    Получение ввода с использованием класса Entry

--- sample/buttom.py
+++ sample/buttom.py
@@ -1,13 +1,20 @@
 from tkinter import *
 
+def clicked():
+    res = "Привет {}".format(txt.get())
+    lbl.configure(text=res)
+
 window = Tk()
 window.title("Добро пожаловать в приложение PythonRu")
 
 lbl = Label(window,text="Привет",font=("Arial Bold",50))
 lbl.grid(column=0,row=0)
 
-btn = Button(window,text="Не нажимать!")
-btn.grid(column=1,row=0)
+txt = Entry(window,width=10)
+txt.grid(column=1,row=0)
+
+btn = Button(window, text="Не нажимать!", bg="black", fg="red", command=clicked)
+btn.grid(column=2,row=0)
 
 window.geometry('400x250')
 window.mainloop()
